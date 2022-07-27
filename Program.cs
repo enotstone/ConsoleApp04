07cc760d170cb32606f97f19ffe2541f47ab820f
enot demiurge <enotstone@ya.ru>
2022-07-27 15:17:49 +0300

    12 act

--- Program.cs
+++ Program.cs
@@ -10,6 +10,43 @@ namespace ConsoleApp04
     {
         static void Main(string[] args)
         {
+            #region //Act 12
+
+            Console.WriteLine("Введите радиус для вычисления длинны окружности");
+            Сircle.CirLC(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите радиус для вычисления площади круга");
+            Сircle.CirSC(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите радиус, координаты точки Х и У, для определения находится ли точка в пределах окружности");
+            Сircle.PointToCir(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+            Console.ReadKey();
+            #endregion
+
+
         }
+        static class Сircle
+        {
+
+
+            internal static void CirLC(int R)
+            {
+                Console.WriteLine("Длинна окружности: {0}", (2 * Math.PI * R));
+            }
+            internal static void CirSC(int R)
+            {
+                Console.WriteLine("Площадь круга: {0}", (Math.PI * R * R));
+            }
+            internal static void PointToCir(int R, int Point1, int Point2)
+            {
+                if ((R * R) >= (Point1 * Point1 + Point2 * Point2))
+                {
+                    Console.WriteLine("Точка принадлежит радиусу окружности");
+                }
+                else
+                {
+                    Console.WriteLine("Точка не принадлежит радиусу окружности");
+                }
+            }
+        }
+    
     }
 }
